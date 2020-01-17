eb21c086e870485ab4f901a16f3b9a0384add864
Ernur Kurmanbekov <erazavr85@gmail.com>
2020-01-17 18:19:19 +0600

    used combineReducers

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware, compose} from "redux";
+import {createStore, applyMiddleware, compose,combineReducers} from "redux";
 import {Provider} from 'react-redux'
 import thunk from "redux-thunk";
-import reducer from "./store/reducer";
+import cartReducer from "./store/reducers/cartReducer";
 import App from './App';
 import './index.css';
+import dishesReducer from "./store/reducers/dishesReducer";
+import orderReducer from "./store/reducers/orderReducer";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+    dishes: dishesReducer,
+    cart: cartReducer,
+    order: orderReducer,
+});
 const store = createStore(
-    reducer,
-    composeEnhancers(applyMiddleware(thunk)))
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
     <Provider store={store}>
